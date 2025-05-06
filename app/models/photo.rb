ec92f2139b2ed146c2fb164ecf49b1e6717a95d1
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-05-06 21:31:14 +0000

    Completed Like associations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -26,8 +26,10 @@ class Photo < ApplicationRecord
   has_many(:comments, class_name:"Comment", foreign_key:"photo_id", primary_key:"id")
 
   # Photo#likes: returns rows from the likes table associated to this photo by the photo_id column
+  has_many(:likes, class_name:"Like", foreign_key:"photo_id", primary_key:"id")
 
   ## Indirect associations
 
   # Photo#fans: returns rows from the users table associated to this photo through its likes
+  has_many(:fans, through: :likes, source: :user)
 end
